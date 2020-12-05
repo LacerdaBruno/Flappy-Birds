932bb0c4d6e566db878a5351620803d96ea1b1d1
BRUNO FERNANDO LACERDA DE OLIVEIRA <57118407+LacerdaBruno@users.noreply.github.com>
2020-12-05 09:47:19 -0300

    05/12/2020
    fim do jogo

--- js/flappy.js
+++ js/flappy.js
@@ -54,7 +54,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
             par.setX(par.getX() - deslocamento)
 
             /// quando o elemento sair sa area do jogo
-            if (par.getX() < -par.getLargura()) {
+            if (par.getX() < - par.getLargura()) {
                 par.setX(par.getX() + espaco * this.pares.length)
                 par.sortearAbertura()
             }
@@ -104,6 +104,30 @@ function Progresso() {
 
 }
 
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(ParDeBarreiras => {
+        if (!colidiu) {
+            const superior = ParDeBarreiras.superior.elemento
+            const inferior = ParDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior)
+                ||estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
+
 function FlappyBird() {
     let pontos = 0
 
@@ -126,6 +150,10 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         },20)
     }
 }
